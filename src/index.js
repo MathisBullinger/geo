7ab753d11821a700d9cf4efa740cd9568eb19048
Mathis Bullinger <mathis@bullinger.dev>
2020-08-27 01:40:34 +0200

    feat: render in canvas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import * as d3 from 'd3'
 
 let width = window.innerWidth
 let height = window.innerHeight
-const sensitivity = 75
 
 let projection = d3
   .geoOrthographic()
@@ -11,66 +10,78 @@ let projection = d3
   .rotate([0, -30])
   .translate([width / 2, height / 2])
 
-const initialScale = projection.scale()
-let path = d3.geoPath().projection(projection)
-
-let svg = d3
-  .select('#map')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
-
-let globe = svg
-  .append('circle')
-  .attr('fill', '#222')
-  .attr('stroke-width', '0.2')
-  .attr('cx', width / 2)
-  .attr('cy', height / 2)
-  .attr('r', initialScale)
-
-svg
-  .call(
-    d3.drag().on('drag', () => {
-      const rotate = projection.rotate()
-      const k = sensitivity / projection.scale()
-      projection.rotate([
-        rotate[0] + d3.event.dx * k,
-        rotate[1] - d3.event.dy * k,
-      ])
-      path = d3.geoPath().projection(projection)
-      svg.selectAll('path').attr('d', path)
-    })
-  )
-  .call(
-    d3.zoom().on('zoom', () => {
-      if (d3.event.transform.k > 0.3) {
-        projection.scale(initialScale * d3.event.transform.k)
-        path = d3.geoPath().projection(projection)
-        svg.selectAll('path').attr('d', path)
-        globe.attr('r', projection.scale())
-      } else {
-        d3.event.transform.k = 0.3
-      }
-    })
-  )
-
-let map = svg.append('g')
-
 ;(async () => {
+  const canvas = document.querySelector('canvas')
+  const ctx = canvas.getContext('2d')
+
+  canvas.width = canvas.offsetWidth * devicePixelRatio
+  canvas.height = canvas.offsetHeight * devicePixelRatio
+
+  const pathGenerator = d3.geoPath(projection, ctx)
+
   const data = await d3.json(
     'https://raw.githubusercontent.com/michael-keith/mps_interests/master/view/js/charts/data/world_map.json'
   )
 
-  map
-    .append('g')
-    .attr('class', 'countries')
-    .selectAll('path')
-    .data(data.features)
-    .enter()
-    .append('path')
-    .attr('class', (d) => 'country_' + d.properties.name.replace(' ', '_'))
-    .attr('d', path)
-    .attr('fill', '#fff')
-    .style('stroke', '#222')
-    .style('stroke-width', 0.3)
+  const size = Math.min(canvas.width, canvas.height) * 0.9
+  projection.scale(size / 2)
+  projection.translate([canvas.width / 2, canvas.height / 2])
+  const initialScale = projection.scale()
+
+  ctx.strokeStyle = '#fff'
+
+  function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = '#222'
+    const radius = projection.scale()
+    ctx.ellipse(
+      canvas.width / 2,
+      canvas.height / 2,
+      radius,
+      radius,
+      0,
+      0,
+      2 * Math.PI
+    )
+    ctx.fill()
+    ctx.fillStyle = '#999'
+    ctx.beginPath()
+    pathGenerator(data)
+    ctx.fill()
+    ctx.stroke()
+  }
+
+  function onDrag({ movementX: x, movementY: y }) {
+    const rotate = projection.rotate()
+    const k = 90 / projection.scale()
+    projection.rotate([rotate[0] + x * k, rotate[1] - y * k])
+  }
+
+  canvas.addEventListener('mousedown', () => {
+    window.addEventListener('mousemove', onDrag, { passive: true })
+  })
+  ;['mouseup', 'mouseleave'].forEach((event) => {
+    canvas.addEventListener(event, () => {
+      window.removeEventListener('mousemove', onDrag)
+    })
+  })
+
+  canvas.addEventListener('mousewheel', (e) => {
+    const scale = projection.scale()
+    let next = scale - (e.deltaY / 750) * scale
+    if (next < 0.5 * initialScale) {
+      next = 0.5 * initialScale
+      e.preventDefault()
+    } else if (next > 3 * initialScale) {
+      next = 3 * initialScale
+      e.preventDefault()
+    }
+    projection.scale(next)
+  })
+
+  const nextRen = () => {
+    render()
+    requestAnimationFrame(nextRen)
+  }
+  nextRen()
 })()
