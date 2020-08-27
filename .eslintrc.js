c0fc766c0a6c1f6b72a6ec1a929bd95ce4b1b78b
Mathis Bullinger <mathis@bullinger.dev>
2020-08-27 15:52:31 +0200

    ci: add prettier & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended'],
+  rules: {},
+}
